bf19e6e4978c307ac7c40848a4cf0358247d6c79
Viktor Jordanoski <viktorj560@gmail.com>
2024-05-07 21:47:51 +0200

    feat: C1 end

--- c1/index.js
+++ c1/index.js
@@ -4,6 +4,24 @@ const apiKey = process.env.API_KEY;
 const dbUser = process.env.DB_USERNAME;
 const dbPass = process.env.DB_PASSWORD;
 
-console.log(`API Key: ${apiKey}`);
-console.log(`Database username: ${dbUser}`);
-console.log(`Database password: ${dbPass}`);
+// console.log(`API Key: ${apiKey}`);
+// console.log(`Database username: ${dbUser}`);
+// console.log(`Database password: ${dbPass}`);
+
+const express = require("express");
+
+const config = require("./pkg/config");
+const { getForCity, getFiveDaysForecast } = require("./handlers/weather");
+
+const api = express();
+
+api.get("/api/v1/weather/:city", getForCity);
+api.get("/api/v1/forecast/:lat/:lon", getFiveDaysForecast);
+
+api.listen(config.getSection("weather").port, (err) => {
+    err 
+        ? console.log(err)
+        : console.log(
+            `Server started on port ${config.getSection("weather").port}`
+        )
+});
